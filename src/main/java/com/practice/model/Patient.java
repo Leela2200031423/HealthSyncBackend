42a212046fcad34c5742b6e67287a709c43812b5
Leela2200031423 <2200031423@kluniversity.in>
2026-03-04 12:01:46 +0530

    Appointment Model

--- src/main/java/com/practice/model/Patient.java
+++ src/main/java/com/practice/model/Patient.java
@@ -1,10 +1,14 @@
 package com.practice.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,10 +29,8 @@ public class Patient {
 	private String phno;
 	@Column(nullable = false, length = 20)
 	private String password;
-	
-	public Patient() {
-		
-	}
+	@OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
+    private List<Appointment> appointments;
 	public int getId() {
 		return id;
 	}
@@ -71,9 +73,16 @@ public class Patient {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public List<Appointment> getAppointments() {
+		return appointments;
+	}
+	public void setAppointments(List<Appointment> appointments) {
+		this.appointments = appointments;
+	}
 	@Override
 	public String toString() {
 		return "Patient [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", email=" + email
-				+ ", phno=" + phno + ", password=" + password + "]";
+				+ ", phno=" + phno + ", password=" + password + ", appointments=" + appointments + "]";
 	}
+	
 }
---
+++ src/main/java/com/practice/model/Status.java
@@ -0,0 +1,7 @@
+package com.practice.model;
+
+public enum Status {
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
