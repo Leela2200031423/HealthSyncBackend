1d84965bbe9037327711af7539d00915b0ea00c7
Venkat Aravind <venkataravind.123456@gmail.com>
2026-03-12 18:21:39 +0530

    Added Patient Profile with Update Operation

--- src/main/java/com/practice/service/PatientServiceImpl.java
+++ src/main/java/com/practice/service/PatientServiceImpl.java
@@ -1,6 +1,8 @@
 package com.practice.service;
 
+import com.practice.model.Appointment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import com.practice.model.Patient;
 import com.practice.repository.PatientRepo;
+
+import java.util.List;
 
 @Service
 public class PatientServiceImpl implements PatientService {
@@ -31,18 +35,49 @@ public class PatientServiceImpl implements PatientService {
         return patientRepo.save(p);
     }
 
-    @Override
-    public String verify(String email, String password) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+//    @Override
+//    public String verify(String email, String password) {
+//        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+//
+//        if(authentication.isAuthenticated()){
+//            return jwtService.generateToken(email);
+//        }
+//        return "FAiled";
+//    }
+@Override
+public String verify(String email, String password) {
+    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
 
-        if(authentication.isAuthenticated()){
-            return jwtService.generateToken(email);
-        }
-        return "FAiled";
+    if(authentication.isAuthenticated()){
+        Patient patient = patientRepo.findByEmail(email);
+        return jwtService.generateToken(email,"ROLE_PATIENT",patient.getId());
     }
+    return "FAiled";
+}
 
     @Override
     public Patient getPatientByUsername(String username) {
         return patientRepo.findByEmail(username);
     }
+
+    @Override
+    public Patient getProfile(int patientId) {
+        Patient patient = patientRepo.findById(patientId).orElseThrow();
+        return patient;
+    }
+
+    @Override
+    public Patient updateProfile(int patientId,Patient patient) {
+        Patient existingPatient = patientRepo.findById(patientId).orElseThrow();
+        existingPatient.setName(patient.getName());
+        existingPatient.setAge(patient.getAge());
+        existingPatient.setGender(patient.getGender());
+        existingPatient.setPhno(patient.getPhno());
+        return patientRepo.save(existingPatient);
+    }
+
+//    @Override
+//    public List<Appointment> getAllAppointments() {
+//        return
+//    }
 }
